c9a7d407ac870d1fa80caa0492b7db1d043ce1f0
sehomi <sehomi755@gmail.com>
2020-11-02 01:54:14 +0330

    good initial results

---
+++ test.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import numpy as np
+import skimage
+import cv2 as cv2
+from matplotlib import pyplot as plt
+from skimage import data, segmentation, color
+from skimage.future import graph
+import os
+import sys
+from tensorflow import keras
+import pickle
+import autoColorDetection as acd
+
+
+def resizeImg(img, hd, wd):
+
+    w = 0
+    h = 0
+
+    if img.shape[0] > img.shape[1]:
+        h = hd
+        w = int(float(hd) * img.shape[1] / img.shape[0])
+    else:
+        w = wd
+        h = int(float(wd) * img.shape[0] / img.shape[1])
+
+    return cv2.resize(img, (w,h))
+
+def predict(x):
+    global model, scaler
+
+    X = np.array([x])
+    X = scaler.transform(X)
+
+    res = model.predict(X)
+
+    return res[0,0] > 0.3
+
+model = keras.models.load_model('model')
+scaler = pickle.load(open('scaler.pkl','rb'))
+colorDetector = acd.AutoColorDetector()
+
+
+bld_count = 1
+while True:
+    sample_file_name = "dataset/association/raw/bld{:d}_0.jpg".format(bld_count)
+    if os.path.isfile(sample_file_name):
+        sample = cv2.imread(sample_file_name, cv2.COLOR_BGR2RGB)
+        sample = resizeImg(sample, 240, 320)
+
+
+        labels1, masks1, fts1 = colorDetector.detectBuildingColor(sample)
+
+
+        bld_count1 = 1
+        while True:
+
+            current_file_name = "dataset/association/raw/bld{:d}_{:d}.jpg".format(bld_count, bld_count1)
+            if os.path.isfile(current_file_name):
+                current = cv2.imread(current_file_name, cv2.COLOR_BGR2RGB)
+                current = resizeImg(current, 240, 320)
+
+                labels2, masks2, fts2 = colorDetector.detectBuildingColor(current)
+
+                copy = current.copy()
+                copy = cv2.resize(copy, (2*copy.shape[1], 2*copy.shape[0]))
+
+                for i in range(len(masks1)):
+                    for j in range(len(masks2)):
+
+                        if i == j:
+                            continue
+
+                        testFts = np.hstack((fts1[i], fts2[j]))
+
+                        if predict(testFts):
+
+                            red = np.zeros((masks2[j].shape[0], masks2[j].shape[1], 3), dtype=np.uint8)
+                            red[masks2[j] == 255] = (0,0,255)
+                            red = cv2.resize(red, (2*red.shape[1], 2*red.shape[0]))
+                            copy = cv2.addWeighted(copy, 1, red, 0.4, 0.0)
+                            # img1 = cv2.bitwise_and(sample,sample,mask = masks1[i])
+                            # img2 = cv2.bitwise_and(current,current,mask = masks2[j])
+                            # cv2.imshow("img1", img1)
+                            # cv2.imshow("img2", img2)
+                            # cv2.waitKey()
+                cv2.imshow("sample", sample)
+                cv2.imshow("copy", copy)
+                cv2.waitKey()
+
+            else:
+                break
+            bld_count1 = bld_count1 + 1
+
+    else:
+        break
+
+    bld_count = bld_count + 1
--- train_keras.py
+++ train_keras.py
@@ -1,43 +1,225 @@
 #!/usr/bin/env python
 
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import keras
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import SGD
-from matplotlib import pyplot
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
+import sklearn
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import pickle
+import os
+import tempfile
+import seaborn as sns
+
+mpl.rcParams['figure.figsize'] = (12, 10)
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
 
 data = np.loadtxt("dataset/association/features/data1.csv", delimiter=",")
 
-X = data[:,0:16]
-y  = data[:,16]
+train_data, test_data = train_test_split(data, test_size=0.2)
+train_data, val_data = train_test_split(train_data, test_size=0.2)
 
-# split into train and test
-n_train = int(0.8 * y.shape[0])
-trainX, testX = X[:n_train, :], X[n_train:, :]
-trainy, testy = y[:n_train], y[n_train:]
+trainy = np.array(train_data[:,16])
+bool_trainy = trainy != 0
+valy = np.array(val_data[:,16])
+testy = np.array(test_data[:,16])
 
-# define model
-model = Sequential()
-model.add(Dense(50, input_dim=16, activation='relu', kernel_initializer='he_uniform'))
-model.add(Dense(1, activation='sigmoid'))
-opt = SGD(lr=0.01, momentum=0.9)
-model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
-# fit model
-history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=200, verbose=0)
-# evaluate the model
-_, train_acc = model.evaluate(trainX, trainy, verbose=0)
-_, test_acc = model.evaluate(testX, testy, verbose=0)
-print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
-# plot loss during training
-pyplot.subplot(211)
-pyplot.title('Loss')
-pyplot.plot(history.history['loss'], label='train')
-pyplot.plot(history.history['val_loss'], label='test')
-pyplot.legend()
-# plot accuracy during training
-pyplot.subplot(212)
-pyplot.title('Accuracy')
-pyplot.plot(history.history['accuracy'], label='train')
-pyplot.plot(history.history['val_accuracy'], label='test')
-pyplot.legend()
-pyplot.show()
+trainX = np.array(train_data[:,0:16])
+valX = np.array(val_data[:,0:16])
+testX = np.array(test_data[:,0:16])
+
+scaler = StandardScaler()
+trainX = scaler.fit_transform(trainX)
+
+pickle.dump(scaler, open('scaler.pkl','wb'))
+
+valX = scaler.transform(valX)
+testX = scaler.transform(testX)
+
+trainX = np.clip(trainX, -5, 5)
+valX = np.clip(valX, -5, 5)
+testX = np.clip(testX, -5, 5)
+
+
+print('Training labels shape:', trainy.shape)
+print('Validation labels shape:', valy.shape)
+print('Test labels shape:', testy.shape)
+
+print('Training features shape:', trainX.shape)
+print('Validation features shape:', valX.shape)
+print('Test features shape:', testX.shape)
+
+METRICS = [
+      keras.metrics.TruePositives(name='tp'),
+      keras.metrics.FalsePositives(name='fp'),
+      keras.metrics.TrueNegatives(name='tn'),
+      keras.metrics.FalseNegatives(name='fn'),
+      keras.metrics.BinaryAccuracy(name='accuracy'),
+      keras.metrics.Precision(name='precision'),
+      keras.metrics.Recall(name='recall'),
+      keras.metrics.AUC(name='auc'),
+]
+
+def plot_loss(history, label, n):
+  # Use a log scale to show the wide range of values.
+  plt.semilogy(history.epoch,  history.history['loss'],
+               color=colors[n], label='Train '+label)
+  plt.semilogy(history.epoch,  history.history['val_loss'],
+          color=colors[n], label='Val '+label,
+          linestyle="--")
+  plt.xlabel('Epoch')
+  plt.ylabel('Loss')
+
+  plt.legend()
+
+def plot_roc(name, labels, predictions, **kwargs):
+  fp, tp, _ = sklearn.metrics.roc_curve(labels, predictions)
+
+  plt.plot(100*fp, 100*tp, label=name, linewidth=2, **kwargs)
+  plt.xlabel('False positives [%]')
+  plt.ylabel('True positives [%]')
+  plt.xlim([-0.5,20])
+  plt.ylim([80,100.5])
+  plt.grid(True)
+  ax = plt.gca()
+  ax.set_aspect('equal')
+
+
+def plot_metrics(history):
+  metrics =  ['loss', 'auc', 'precision', 'recall']
+  for n, metric in enumerate(metrics):
+    name = metric.replace("_"," ").capitalize()
+    plt.subplot(2,2,n+1)
+    plt.plot(history.epoch,  history.history[metric], color=colors[0], label='Train')
+    plt.plot(history.epoch, history.history['val_'+metric],
+             color=colors[0], linestyle="--", label='Val')
+    plt.xlabel('Epoch')
+    plt.ylabel(name)
+    if metric == 'loss':
+      plt.ylim([0, plt.ylim()[1]])
+    elif metric == 'auc':
+      plt.ylim([0.8,1])
+    else:
+      plt.ylim([0,1])
+
+    plt.legend()
+
+def plot_cm(labels, predictions, p=0.5):
+  cm = confusion_matrix(labels, predictions > p)
+  plt.figure(figsize=(5,5))
+  sns.heatmap(cm, annot=True, fmt="d")
+  plt.title('Confusion matrix @{:.2f}'.format(p))
+  plt.ylabel('Actual label')
+  plt.xlabel('Predicted label')
+
+  print('Legitimate Transactions Detected (True Negatives): ', cm[0][0])
+  print('Legitimate Transactions Incorrectly Detected (False Positives): ', cm[0][1])
+  print('Fraudulent Transactions Missed (False Negatives): ', cm[1][0])
+  print('Fraudulent Transactions Detected (True Positives): ', cm[1][1])
+  print('Total Fraudulent Transactions: ', np.sum(cm[1]))
+
+
+def make_model(metrics = METRICS, output_bias=None):
+  if output_bias is not None:
+     output_bias = tf.keras.initializers.Constant(output_bias)
+     model = keras.Sequential([
+          keras.layers.Dense(
+              32, activation='relu',
+              input_shape=(trainX.shape[-1],)),
+          keras.layers.Dropout(0.5),
+          keras.layers.Dense(1, activation='sigmoid',
+                             bias_initializer=output_bias),
+      ])
+  else:
+     model = keras.Sequential([
+          keras.layers.Dense(
+              64, activation='relu',
+              input_shape=(trainX.shape[-1],)),
+          keras.layers.Dropout(0.5),
+          keras.layers.Dense(1, activation='sigmoid'),
+      ])
+
+  model.compile(
+      optimizer=keras.optimizers.Adam(lr=1e-3),
+      loss="binary_crossentropy",
+      metrics=metrics)
+
+  return model
+
+EPOCHS = 100
+BATCH_SIZE = 1024
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_auc',
+    verbose=1,
+    patience=10,
+    mode='max',
+    restore_best_weights=True)
+
+pos = np.count_nonzero(data[:,16] == 1)
+neg = np.count_nonzero(data[:,16] == 0)
+total = data.shape[0]
+
+weight_for_0 = (1 / neg)*(total)/2.0
+weight_for_1 = (1 / pos)*(total)/2.0
+
+
+class_weight = {0: weight_for_0, 1: weight_for_1}
+
+print('Weight for class 0: {:.2f}'.format(weight_for_0))
+print('Weight for class 1: {:.2f}'.format(weight_for_1))
+
+
+b0 = np.log([pos/neg])
+print(pos, neg, b0)
+
+model = make_model(output_bias=b0)
+model.summary()
+
+initial_weights = os.path.join(tempfile.mkdtemp(),'initial_weights')
+model.save_weights(initial_weights)
+
+
+model.load_weights(initial_weights)
+baseline_history = model.fit(
+    trainX,
+    trainy,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    callbacks = [early_stopping],
+    validation_data=(valX, valy),
+    # class_weight=class_weight)
+    )
+
+
+# plot_loss(baseline_history, "", 0)
+plot_metrics(baseline_history)
+
+test_predictions_baseline = model.predict(testX, batch_size=BATCH_SIZE)
+train_predictions_baseline = model.predict(trainX, batch_size=BATCH_SIZE)
+
+plot_cm(testy, test_predictions_baseline)
+# plot_roc("Train Baseline", trainy, train_predictions_baseline, color=colors[0])
+# plot_roc("Test Baseline", testy, test_predictions_baseline, color=colors[0], linestyle='--')
+plt.legend(loc='lower right')
+
+
+baseline_results = model.evaluate(testX, testy,
+                                  batch_size=BATCH_SIZE, verbose=0)
+for name, value in zip(model.metrics_names, baseline_results):
+  print(name, ': ', value)
+print()
+
+model.save('model')
+
+plt.show()
